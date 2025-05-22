489ad78adde78a8d09ea3333886d4c2e63ed311d
LauraAG98 <lauraguirregarcia@outlook.com>
2025-05-21 22:51:11 -0500

    Se llama la funcion y se muestra la cantidad de archivos

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ async function procesoArchivos(){
     const nombre=prompt("Ingrese su nombre por favor");
     const cant=parseInt("Ingrese la cantidad de archivos que desea generar");
 }
+
+
+
+
+
+
+
+
+console.log(`Este es el número total de archivos generados: ${cant}`);
+
+procesoArchivos();
