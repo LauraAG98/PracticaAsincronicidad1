d1480d3b93fc4f5f872d307d0585996f29566fba
LauraAG98 <lauraguirregarcia@outlook.com>
2025-05-21 22:38:25 -0500

    Se crean constantes para nombre y cantidad de archivos

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const arch= require('fs').promises;
+
+async function procesoArchivos(){
+    const nombre=prompt("Ingrese su nombre por favor");
+    const cant=parseInt("Ingrese la cantidad de archivos que desea generar");
+}
