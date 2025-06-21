0f3826f3a10a21489cdc6be47b496ac3860f87bd
LauraAG98 <lauraguirregarcia@outlook.com>
2025-06-20 19:59:41 -0500

    Modificacion de codigo

--- codigoIndex/index.js
+++ codigoIndex/index.js
@@ -1,35 +1,32 @@
-//Se importan las funciones de promesas del sistema de archivos(fs) para trabajar de forma asincrona.
-const arch= require('fs').promises;
+const { error } = require('console');
+const { performServerHandshake } = require('http2');
 
-//Se importa prompt para que el usuario pueda ingresar datos desde la consola.
+const arch= require('fs').promises;
 const prompt=require('prompt-sync')();
 
-//Se  agrega funcion asincrona
-async function procesoArchivos(){
+function procesoArchivos(){
 
-//Se le pide al usuario nombre y cantidad de archivos
+
     const nombre=prompt("Ingrese su nombre por favor");
     const cant=parseInt(prompt("Ingrese la cantidad de archivos que desea generar"));
 
-//Ciclo que crea la cantidad de archivos ingresada por el usuario 
+    const promesas=[];
+    
     for(let i =1; i<= cant ;i++){
-
-//Se define el contenido de cada archivo, se muestra nombre del usuario y numero de archivo
     const contenido=`Hola ${nombre}, este es el archivo número ${i}`;
-
-//Se define una constante para guardar archivo por archivo 
     const nomArch=`archivo ${i}.txt`;
 
-//Se usa await para esperar que se vaya creando cada archivo con WriteFile antes de continuar.   
-    await arch.writeFile(nomArch, contenido);
-
-//Se imprime en consola el nombre del archivo creado 
     console.log(`Archivo creado: ${nomArch}`);
+
+    const generador= arch.writeFile(nomArch,contenido);
+    promesas.push(generador);
     }
 
-//Se imprime la cantidad de archivos generados
-    console.log(`Este es el número total de archivos generados: ${cant}`);
+    Promise.all(promesas)
+        .then(()=>{
+            console.log(`Este es el número total de archivos generados: ${cant}`);
+        }).catch((error)=>{
+             console.log(`Error al crear los archivos`,error);
+        });
 }
-
-//Se llama la función para que ejecute todo el código
 procesoArchivos();
